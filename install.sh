2bc1afa9817c218faef05e5605e5f69a1b316e50
toxic0berliner <toxicpublic@gmail.com>
2020-11-26 10:50:01 +0100

    removing hyphens since it was not working with them, trying without. Also fixed file ownership in install.

--- install.sh
+++ install.sh
@@ -14,12 +14,13 @@ echo "install some dependencies : bash"
 pkg install bash
 
 echo "installing our script"
-mv $scriptdir/wifi-watchdog.sh /usr/local/bin/
-chmod +x /usr/local/bin/wifi-watchdog.sh
+mv $scriptdir/wifiwatchdog.sh /usr/local/bin/
+chmod +x /usr/local/bin/wifiwatchdog.sh
+chown root:wheel /usr/local/bin/wifiwatchdog.sh
 
 echo "installing the cron task"
-mv $scriptdir/actions_wifi-watchdog.conf /usr/local/opnsense/service/conf/actions.d/
-chown root:wheels /usr/local/opnsense/service/conf/actions.d/actions_wifi-watchdog.conf
+mv $scriptdir/actions_wifiwatchdog.conf /usr/local/opnsense/service/conf/actions.d/
+chown root:wheel /usr/local/opnsense/service/conf/actions.d/actions_wifiwatchdog.conf
 
 echo "done. It seems a reboot is necessary for the cron action to show up in the GUI."
 
